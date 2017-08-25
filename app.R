e1ae29f59129a4cc14288f646516e48b9d744c8b
LCULaval <laurent.m.caron@gmail.com>
2017-08-25 14:44:29 -0400

    Creating plot_image_matrix.R

--- app.R
+++ app.R
@@ -42,6 +42,8 @@ server <- function(input, output, session){
     ### sourcing ###
     
     source("src/interpret_results.R")
+    source("src/plot_image_matrix.R")
+    source("src/image_matrix.R")
     
     ### Generate Background Grid ###
     
@@ -123,18 +125,7 @@ server <- function(input, output, session){
         req(input_digit())
         
         input_digit() %>% 
-            "["(1, 28:1,,) %>% 
-            melt() %>% 
-            ggplot(aes(x = X2, y = X1, fill = value)) + 
-            geom_tile() +
-            scale_fill_gradient(low = "white", high = "black") +
-            theme(axis.title.x=element_blank(),
-                  axis.text.x=element_blank(),
-                  axis.ticks.x=element_blank(),
-                  axis.title.y=element_blank(),
-                  axis.text.y=element_blank(),
-                  axis.ticks.y=element_blank(),
-                  legend.position = "none")
+            plot_image_matrix()
     }, width = 280, height = 280)
 
 }
